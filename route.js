3bf0fa1fbff7e319c6de10aaf069a3ee5d759229
CLINTON <clintonchiemeka@gmail.com>
2024-11-28 08:36:58 +0000

    Rename function to base64EncodeCustom

--- route.js
+++ route.js
@@ -3,7 +3,7 @@ const { base64Encode, getDocs } = require('./controller');
 
 const router = express.Router();
 
-router.post('/base64Encode', base64Encode);
-router.get('/base64Encode', getDocs); 
+router.post('/base64EncodeCustom', base64Encode);
+router.get('/base64EncodeCustom', getDocs); 
 
 module.exports = router;
