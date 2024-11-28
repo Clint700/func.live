9b993fcd42135107b03c6aabaabaf408c0957cd2
CLINTON <clintonchiemeka@gmail.com>
2024-11-28 08:33:52 +0000

    Rename function to base64EncodeCustom

--- controller.js
+++ controller.js
@@ -1,22 +1,22 @@
-const { encodeToBase64 } = require('./model');
+const { encodeToBase64 } = require("./model");
 
 const base64Encode = async (req, res) => {
   try {
     const { input } = req.body;
     if (!input) {
-      return res.status(400).json({ error: 'Input is required' });
+      return res.status(400).json({ error: "Input is required" });
     }
     const output = encodeToBase64(input);
     res.json({ output });
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({ error: "An error occurred" });
   }
 };
 
 const getDocs = async (req, res) => {
   const docs = {
-    name: "base64Encode",
-    description: "Encode anything to base64",
+    name: "base64EncodeCustom",
+    description: "Custom function to encode anything to base64",
     input: {
       type: "string",
       description: "Input the data you'd like to encode to base64",
